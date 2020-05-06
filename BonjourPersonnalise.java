1d9a53ed03adf48060e4633692916942a10e5121
jmdroza <droza.jessica@gmail.com>
2020-05-06 10:55:08 +0200

    TP cours java

---
+++ BonjourPersonnalise.java
@@ -0,0 +1,14 @@
+import java.util.Scanner; 
+import java.util.*;
+
+public class BonjourPersonnalise {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+		System.out.println("Bonjour " + str);
+
+	}
+
+}
