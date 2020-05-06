5c1cc1c354626745592e9ed40afd145a94fe7150
jmdroza <droza.jessica@gmail.com>
2020-05-06 11:00:55 +0200

    Ajout de commantaire

--- Bonjour.java
+++ Bonjour.java
@@ -1,9 +1,13 @@
+/**
+*@author JDRO
+*
+**/
 public class Bonjour {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//Ajout de commentaire
 		System.out.println("Hello-world");
-
+		
 	}
 
 }
--- BonjourPersonnalise.java
+++ BonjourPersonnalise.java
@@ -4,7 +4,6 @@ import java.util.*;
 public class BonjourPersonnalise {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 		String str = sc.nextLine();
 		System.out.println("Bonjour " + str);
