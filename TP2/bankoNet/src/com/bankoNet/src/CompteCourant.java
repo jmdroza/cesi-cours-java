abb99a2ad4db9a6e2dc4e67a0301438cd5d1a913
jmdroza <droza.jessica@gmail.com>
2020-05-07 11:42:47 +0200

    AJout de test debiter crediter et attribut prive

--- TP2/bankoNet/src/com/bankoNet/src/CompteCourant.java
+++ TP2/bankoNet/src/com/bankoNet/src/CompteCourant.java
@@ -1,30 +1,56 @@
 package com.bankoNet.src;
 
 public class CompteCourant {
-	public String numero;
-	public String intitule;
-	public double solde;
-	public double montantDecouvertAutorise;
+	private String numero;
+	private String intitule;
+	private double solde;
+	private double montantDecouvertAutorise;
 	public static int nbCompteCourant = 0;
 	
+	public CompteCourant() {
+		nbCompteCourant++;
+	}
+	
 	public CompteCourant(String numero, String intitule, double solde, double montantDecouvertAutorise) {
+		this();
+		if(solde < 0) {
+			System.out.println("On ne peut pas creer de compte avec un sole negatif.");
+			solde = 0;
+		}else {
+			this.solde = solde;
+		}
 		this.numero = numero;
 		this.intitule = intitule;
 		this.solde = solde;
 		this.montantDecouvertAutorise = montantDecouvertAutorise;
-		nbCompteCourant++;
 	}
 	
+	@Override
+	public String toString() {
+		return "Compte n° " + this.numero + ", intitule : " + this.intitule + " votre solde est de " + this.solde+".";
+	}
+	
+	
 	public void afficher() {
-		System.out.println("Compte n° " + this.numero + ", intitule : " + this.intitule + " votre solde est de " + this.solde+".");
+		System.out.println(this.toString());
 	}
 	
 	public void crediter(double somme) {
-		this.solde += somme;
+		if(somme < 0 ) {
+			System.out.println("Vous ne pouvez pas créditer une somme négative");
+		}else {
+			this.solde += somme;
+		}
+		
 	}
 	
 	public void debiter(double somme) {
-		this.solde -= somme;
+		double soldeTmp = 0;
+		if((soldeTmp = this.solde - somme) < - this.montantDecouvertAutorise) {
+			System.out.println("Opération non-autorisé, la somme a débité dépasse le montant découvert autorisé");
+		}else {
+			this.solde -= somme;
+		}
 	}
 	
 	//Getter & Setter
@@ -48,9 +74,6 @@ public class CompteCourant {
 		return solde;
 	}
 
-	public void setSolde(double solde) {
-		this.solde = solde;
-	}
 
 	public double getMontantDecouvertAutorise() {
 		return montantDecouvertAutorise;
--- TP2/bankoNet/src/com/bankoNet/test/CompteCourantTest.java
+++ TP2/bankoNet/src/com/bankoNet/test/CompteCourantTest.java
@@ -1,41 +1,74 @@
 package com.bankoNet.test;
 import java.util.*;
+import org.junit.Before;
+import org.junit.Test;
+
 import com.bankoNet.src.CompteCourant;
 
 public class CompteCourantTest{
-	@org.junit.Test
-	public void methodeDeTest() {
+	CompteCourant compteCourant1;
+	CompteCourant compteCourant2;
+	CompteCourant compteCourant3;
+	CompteCourant compteCourant4;
+	
+	@Before
+	public void init() {
+		CompteCourant.nbCompteCourant = 0;
+	}
+	
+	@Test
+	public void IncrementCompteCourantTest() {
 		System.out.println("--- Début Test de nbCompteCourant ---");
-		CompteCourant compteCourant1 = new CompteCourant("003","CompteFamille", 1250, 300);
-		CompteCourant compteCourant2 = new CompteCourant("004","CompteEntreprise", 6500, 600);
-		CompteCourant compteCourant3 = new CompteCourant("005","CompteEtude", 250, 50);
-		
+		compteCourant1 = new CompteCourant("003","CompteFamille", 1250, 300);
+		compteCourant2 = new CompteCourant("004","CompteEntreprise", 6500, 600);
+		compteCourant3 = new CompteCourant("005","CompteEtude", 5250, 50);
+		compteCourant4 = new CompteCourant("006","CompteEntreprise", 5400, 300);
 		ArrayList<CompteCourant> listeCompte = new ArrayList<CompteCourant>();
 		listeCompte.add(compteCourant1);
 		listeCompte.add(compteCourant2);
 		listeCompte.add(compteCourant3);
+		listeCompte.add(compteCourant4);
 		
 		for(CompteCourant compte : listeCompte) {
 			compte.afficher();
 		}
-		org.junit.Assert.assertTrue(CompteCourant.getNbCompteCourant() == 3);
+		org.junit.Assert.assertTrue(CompteCourant.getNbCompteCourant() == 4);
 		System.out.println("--- Fin Test de nbCompteCourant ---");
 	}
-	@org.junit.Test
-	public void crediterTest() {
-		System.out.println("--- Début Test de créditer ---");
-		CompteCourant compteCourant1 = new CompteCourant("006","CompteEntreprise", 5400, 300);
+	
+	@Test
+	public void crediterMtPositifTest() {
+		System.out.println("--- Début Test de créditer montant positif ---");
+		compteCourant1 = new CompteCourant("003","CompteFamille", 1250, 300);
 		compteCourant1.crediter(5000);
-		org.junit.Assert.assertTrue(compteCourant1.solde == 10400);
-		System.out.println("--- Fin Test de créditer ---");
+		org.junit.Assert.assertTrue(compteCourant1.getSolde() == 6250);
+		System.out.println("--- Fin Test de créditer montant positif ---");
+	}
+	@Test
+	public void crediterMtNegatifTest() {
+		System.out.println("--- Début Test de créditer montant négatif ---");
+		compteCourant2 = new CompteCourant("003","CompteFamille", 1250, 300);
+		compteCourant2.crediter(-250);
+		org.junit.Assert.assertTrue(compteCourant2.getSolde() == 1250);
+		System.out.println("--- Fin Test de créditer montant négatif ---");
+	}
+
+	
+	@Test
+	public void debiterMtPositifTest() {
+		System.out.println("--- Début Test de débiter montant positif ---");
+		compteCourant3 = new CompteCourant("005","CompteEtude", 5250, 50);
+		compteCourant3.debiter(5000);
+		org.junit.Assert.assertTrue(compteCourant3.getSolde() == 250);
+		System.out.println("--- Fin Test de débiter montant positif ---");
 	}
 	
-	@org.junit.Test
-	public void debiterTest() {
-		System.out.println("--- Début Test de débiter ---");
-		CompteCourant compteCourant1 = new CompteCourant("007","CompteEntreprise", 5400, 300);
-		compteCourant1.debiter(5000);
-		org.junit.Assert.assertTrue(compteCourant1.solde == 400);
-		System.out.println("--- Fin Test de débiter ---");
+	@Test
+	public void debiterDepassementSoldeTest() {
+		System.out.println("--- Début Test de débiter pour dépasser le solde ---");
+		compteCourant4 = new CompteCourant("006","CompteEntreprise", 5400, 300);
+		compteCourant4.debiter(8000);
+		org.junit.Assert.assertTrue(compteCourant4.getSolde() == 5400);
+		System.out.println("--- Fin Test de débiter pour dépasser le solde ---");
 	}
 }
